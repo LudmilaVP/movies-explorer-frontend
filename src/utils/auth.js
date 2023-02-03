7d2a1351140a427214a75e3e457ebb4ad5d2cf3e
LudmilaVP <liudmila.lupandina@yandex.ru>
2023-02-03 20:37:50 +0300

    сделана часть компонентов

---
+++ src/utils/auth.js
@@ -0,0 +1,43 @@
+const BASE_URL = 'https://api.moviexplorer.nomoredomains.rocks';
+
+function getResponse(res) {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Ошибка: ${res.status}`);
+}
+
+export const authorization = (email, password) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  })
+    .then(res => getResponse(res))
+}
+
+export const login = (email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  })
+    .then(res => getResponse(res))
+}
+
+export const logout = () => {
+  return fetch(`${BASE_URL}/onlogout`, {
+    method: "GET",
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  })
+    .then(res => getResponse(res));
+};
