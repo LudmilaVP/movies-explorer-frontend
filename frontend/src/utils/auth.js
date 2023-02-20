1cc5b39a04a0f592b89a75c727e0a8f861a4cd2d
LudmilaVP <liudmila.lupandina@yandex.ru>
2023-02-21 00:03:16 +0300

    декомпозиция

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -7,14 +7,14 @@ function getResponse(res) {
   return Promise.reject(`Ошибка: ${res.status}`);
 }
 
-export const authorization = ({email, password, name}) => {
+export const authorization = ({name, email, password}) => {
   return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
     credentials: 'include',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ email, password, name })
+    body: JSON.stringify({ name, email, password })
   })
     .then(res => getResponse(res))
 }
