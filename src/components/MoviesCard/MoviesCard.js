1f97cd64e7b03bf0ece80a60d235a526b040e52d
LudmilaVP <liudmila.lupandina@yandex.ru>
2023-03-18 19:46:13 +0300

    change css

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -38,18 +38,17 @@ const MoviesCard = ({ film, savedMoviesToggle, filmsSaved }) => {
 
   return (
     <li className="movie">
-      <div className="movie__container" href={pathname === '/saved-movies' ? film.trailer : film.trailerLink} target="_blank"  rel="noreferrer">
-        <img src={pathname === '/saved-movies' ? `${film.image}` : `https://api.nomoreparties.co${film.image.url}`} alt={film.nameRU} className="movie__image"></img>
+      <div className="movie__container" href={pathname === '/saved-movies' ? film.trailer : film.trailerLink} target="_blank" rel="noreferrer">
         <p className="movie__title">{film.nameRU}</p>
         <div className="movie__buttons">
-
-        {pathname === '/saved-movies' ? (
+          {pathname === '/saved-movies' ? (
             <button type="button" className="movie__button movie__button_delete" onClick={handleFavoriteDelete} />
           ) : (
             <button type="button" className={`movie__button movie__button${favorite ? '_active' : '_inactive'}`} onClick={handleFavoriteToogle} />
           )}
         </div>
         <p className="movie__duration">{getMovieDuration(film.duration)}</p>
+        <img src={pathname === '/saved-movies' ? `${film.image}` : `https://api.nomoreparties.co${film.image.url}`} alt={film.nameRU} className="movie__image"></img>
       </div>
     </li>
   );
