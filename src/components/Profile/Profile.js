2b6ff9819e9d662df3732bbe76962a4ad7a9e7e9
LudmilaVP <liudmila.lupandina@yandex.ru>
2023-03-26 22:10:38 +0300

    profile fixing

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,67 +1,52 @@
 import './Profile.css';
-import { useState, useContext } from 'react';
+import { useCallback, useState, useEffect, useContext } from 'react';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
-import mainApi from '../../utils/MainApi';
+import ProfileValidation from '../../utils/ProfileValidation';
 
-const Profile = ({ onSignOut }) => {
+function Profile({ onSignOut, onUpdateProfile }) {
   const currentUser = useContext(CurrentUserContext);
-  const [name, setName] = useState(currentUser.name);
-  const [lastName, setLastName] = useState(currentUser.name);
-  const [email, setEmail] = useState(currentUser.email);
-  const [lastEmail, setLastEmail] = useState(currentUser.email);
-  const [isButton, setButton] = useState(false);
+    const currentUserName = currentUser.name
+    const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
+    const { values, setValues, handleChange, errors, isValid } = ProfileValidation();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const checkStatusSubmit = useCallback(() => {
+        return !isValid || values.name === currentUser.name && values.email === currentUser.email;
 
-    mainApi.setUserProfile({ name, email }).then(() => {
-      setButton(false);
-      setLastName(name);
-      setLastEmail(email);
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-  };
+    }, [isValid, values, currentUser]);
 
-  function handleNameChange(e) {
-    const value = e.target.value;
-    setName(value);
+    useEffect(() => {
+        setValues({ 'name': currentUser.name, 'email': currentUser.email });
+    }, [setValues, currentUser]);
 
-    if (value !== lastName) {
-      setButton(true);
-    } else {
-      setButton(false);
+    useEffect(() => {
+        setIsSubmitDisabled(checkStatusSubmit());
+    }, [checkStatusSubmit]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsSubmitDisabled(true);
+        onUpdateProfile(values);
+        checkStatusSubmit();
     }
-  }
-
-  function handleEmailChange(e) {
-    const value = e.target.value;
-    setEmail(value);
-
-    if (value !== lastEmail) {
-      setButton(true);
-    } else {
-      setButton(false);
-    }
-  }
 
   return (
     <section className="profile">
       <form className="profile__form" onSubmit={handleSubmit}>
-        <h3 className="profile__welcome">Привет, {name}!</h3>
+        <h3 className="profile__welcome">Привет, {currentUserName}!</h3>
         <div className="profile__input">
           <p className="profile__text">Имя</p>
           <div className="profile__field profile__field_type_name">
-            <input className="profile__settings" value={name} onChange={handleNameChange} required />
+            <input className="profile__settings" value={values.name || ''} onChange={handleChange} required />
           </div>
+          <span className='profile__error'>{errors.name || ''}</span>
           <div className="profile__field profile__field_type_email">
-            <input className="profile__settings" value={email} onChange={handleEmailChange} required />
+            <input className="profile__settings" value={values.email || ''} onChange={handleChange} required />
           </div>
           <p className="profile__text">E-mail</p>
+          <span className='profile__error'>{errors.email || ''}</span>
         </div>
-        <button className="profile__button" disabled={!isButton}>Редактировать</button>
-        <button className="profile__link" onClick={onSignOut}>Выйти из аккаунта</button>
+        <button className="profile__button" disabled={isSubmitDisabled}>Редактировать</button>
+        <button className="profile__button profile__button_logout" onClick={onSignOut}>Выйти из аккаунта</button>
       </form>
     </section>
   );
