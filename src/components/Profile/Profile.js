36713c8dab0278c9a6001f2642288f1f747f3f0e
LudmilaVP <liudmila.lupandina@yandex.ru>
2023-03-27 00:49:16 +0300

    fin prof

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,47 +1,31 @@
 import './Profile.css';
 import { useState, useContext} from 'react';
-import { useHistory } from 'react-router-dom';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
-import mainApi from '../../utils/MainApi';
 
-function Profile({ onSignOut }) {
+function Profile({ onSignOut, handleUpdateProfile }) {
   const currentUser = useContext(CurrentUserContext);
-  const [name, setName] = useState(currentUser.name);
-  const [lastName, setLastName] = useState(currentUser.name);
-  const [email, setEmail] = useState(currentUser.email);
-  const [lastEmail, setLastEmail] = useState(currentUser.email);
   const [isButton, setButton] = useState(false);
-  const history = useHistory();
+  const [values, setValues] = useState()
 
-  function handleSubmit() {
-    mainApi.setUserProfile({ name, email })
-    .then(() => {
-      setButton(false);
-      setLastName(name);
-      setLastEmail(email);
-      history.push('/profile')
-    })
-    .catch((err) => {
-      console.log(err.message)
-    });
-  };
-
-  function handleNameChange(e) {
-    const value = e.target.value;
-    setName(value);
-
-    if (value !== lastName) {
-      setButton(true);
-    } else {
-      setButton(false);
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (values.name && values.email && values.password) {
+      handleUpdateProfile(values.name, values.email, values.password)
+    }
+    else if (values.email && values.password){
+      handleUpdateProfile(values.email, values.password)
+    }
+    else {
+      handleUpdateProfile(values.name, values.email)
     }
   }
 
-  function handleEmailChange(e) {
-    const value = e.target.value;
-    setEmail(value);
+  function handleChange(e) {
+    const target = e.target
+    const { name, value } = target
+    setValues({...values, [name]: value})
 
-    if (value !== lastEmail) {
+    if (value !== name) {
       setButton(true);
     } else {
       setButton(false);
@@ -51,15 +35,15 @@ function Profile({ onSignOut }) {
   return (
     <section className="profile">
       <form className="profile__form" onSubmit={handleSubmit}>
-        <h3 className="profile__welcome">Привет, {name}!</h3>
+        <h3 className="profile__welcome">Привет, {currentUser.name}!</h3>
         <div className="profile__input">
           <p className="profile__text">Имя</p>
           <div className="profile__field profile__field_type_name">
-            <input className="profile__settings" value={name} onChange={handleNameChange} required />
+            <input className="profile__settings" value={values?.name} onChange={handleChange} required />
           </div>
 
           <div className="profile__field profile__field_type_email">
-            <input className="profile__settings" value={email} onChange={handleEmailChange} required />
+            <input className="profile__settings" value={values?.email} onChange={handleChange} required />
           </div>
           <p className="profile__text">E-mail</p>
         </div>
