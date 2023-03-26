7221a62ee085e947d7b15f70d94ff1f01091baaa
LudmilaVP <liudmila.lupandina@yandex.ru>
2023-03-26 20:48:08 +0300

    fix profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,64 +6,51 @@ import { CurrentUserContext } from '../../contexts/CurrentUserContext'
 import mainApi from '../../utils/MainApi';
 
 function Profile(props) {
-  const currentUser = useContext(CurrentUserContext);
-  const [name, setName] = useState(currentUser.name);
-  const [lastName, setLastName] = useState(currentUser.name);
-  const [email, setEmail] = useState(currentUser.email);
-  const [lastEmail, setLastEmail] = useState(currentUser.email);
-  const [isButton, setButton] = useState(false);
+  const currentUser = useContext(CurrentUserContext)
+  const [isInputDisabled, setIsInputDisabled] = useState(true)
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  useEffect(() => {
+    setValues(currentUser)
+  }, [currentUser])
 
-    mainApi.setUserProfile({ name, email })
-    .then(() => {
-      setButton(false);
-      setLastName(name);
-      setLastEmail(email);
-    })
-    .catch((err) => {
-      console.log(err.message)
-    });
-  };
-
-  function handleNameChange(e) {
-    const value = e.target.value;
-    setName(value);
-
-    if (value !== lastName) {
-      setButton(true);
-    } else {
-      setButton(false);
-    }
+  function handleEditProfile() {
+    setIsInputDisabled(false)
   }
 
-  function handleEmailChange(e) {
-    const value = e.target.value;
-    setEmail(value);
-
-    if (value !== lastEmail) {
-      setButton(true);
-    } else {
-      setButton(false);
+  const [values, setValues] = useState()
+  const handleChange = e => {
+    const target = e.target
+    const { name, value } = target
+    setValues({...values, [name]: value})
+  }
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (values.name && values.email && values.password) {
+      handleEditProfile(values.name, values.email, values.password)
+    }
+    else if (values.email && values.password){
+      handleEditProfile(values.email, values.password)
+    }
+    else {
+      handleEditProfile(values.name, values.email)
     }
   }
 
   return (
     <section className="profile">
       <form className="profile__form" onSubmit={handleSubmit}>
-        <h3 className="profile__welcome">Привет, {name}!</h3>
+        <h3 className="profile__welcome">Привет, {currentUser.name}!</h3>
         <div className="profile__input">
           <p className="profile__text">Имя</p>
           <div className="profile__field profile__field_type_name">
-            <input className="profile__settings" value={name} onChange={handleNameChange} required />
+            <input className="profile__settings" value={values?.name} onChange={handleChange} disabled={isInputDisabled} required />
           </div>
           <div className="profile__field profile__field_type_email">
-            <input className="profile__settings" value={email} onChange={handleEmailChange} required />
+            <input className="profile__settings" value={values?.email} onChange={handleChange} disabled={isInputDisabled} required />
           </div>
           <p className="profile__text">E-mail</p>
         </div>
-        <Link to="/profile" className="profile__button" disabled={!isButton}>Редактировать</Link>
+        <Link to="/profile" className="profile__button" onClick={handleEditProfile}>Редактировать</Link>
         <Link to="/" className="profile__link" onClick={props.handleSignOut}>Выйти из аккаунта</Link>
       </form>
     </section>
