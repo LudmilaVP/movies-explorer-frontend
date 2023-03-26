bd2922cd36bcd09428c63c2b8e4bf8f0a5bb50a1
LudmilaVP <liudmila.lupandina@yandex.ru>
2023-03-27 01:42:00 +0300

    event

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,42 +1,67 @@
 import './Profile.css';
-import { useContext, useEffect } from 'react';
+import { useContext, useState } from 'react';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
-import useFormValidation from '../../utils/useFormValidation.js';
+import mainApi from '../../utils/MainApi';
 
-function Profile({ onSignOut, handleUpdateProfile }) {
-  const { values, handleChange, resetForm, isValid } = useFormValidation();
-  const currentUser = useContext(CurrentUserContext); 
+function Profile({ onSignOut}) {
+  const currentUser = useContext(CurrentUserContext);
+  const [name, setName] = useState(currentUser.name);
+  const [lastName, setLastName] = useState(currentUser.name);
+  const [email, setEmail] = useState(currentUser.email);
+  const [lastEmail, setLastEmail] = useState(currentUser.email);
+  const [isButton, setButton] = useState(false);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    handleUpdateProfile(values);
+  const handleSubmit = () => {
+    mainApi.setUserProfile({ name, email })
+    .then(() => {
+      setButton(false);
+      setLastName(name);
+      setLastEmail(email);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  };
+
+  function handleNameChange(e) {
+    const value = e.target.value;
+    setName(value);
+
+    if (value !== lastName) {
+      setButton(true);
+    } else {
+      setButton(false);
+    }
   }
 
-  useEffect(() => {
-    if (currentUser) {
-      resetForm(currentUser, {}, true);
-    }
-  }, [currentUser, resetForm]);
+  function handleEmailChange(e) {
+    const value = e.target.value;
+    setEmail(value);
 
-  const requirementValidity = (!isValid || (currentUser.name === values.name && currentUser.email === values.email));
+    if (value !== lastEmail) {
+      setButton(true);
+    } else {
+      setButton(false);
+    }
+  }
 
   return (
     <section className="profile">
       <form className="profile__form" onSubmit={handleSubmit}>
-        <h3 className="profile__welcome">Привет, {currentUser.name}!</h3>
+        <h3 className="profile__welcome">Привет, {name}!</h3>
         <div className="profile__input">
           <p className="profile__text">Имя</p>
           <div className="profile__field profile__field_type_name">
-            <input className="profile__settings" value={values.name} onChange={handleChange} required />
+            <input className="profile__settings" value={name} onChange={handleNameChange} required />
           </div>
 
           <div className="profile__field profile__field_type_email">
-            <input className="profile__settings" value={values.email} onChange={handleChange} required />
+            <input className="profile__settings" value={email} onChange={handleEmailChange} required />
           </div>
           <p className="profile__text">E-mail</p>
         </div>
 
-        <button className="profile__button" disabled={requirementValidity ? true : false}>Редактировать</button>
+        <button className="profile__button" disabled={!isButton}>Редактировать</button>
         <button className="profile__button profile__button_logout" onClick={onSignOut}>Выйти из аккаунта</button>
 
       </form>
